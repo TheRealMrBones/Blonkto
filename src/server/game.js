bdd3e6c88113420975035fdbde387bb5215b5370
MrBones <62398642+TheRealMrBones@users.noreply.github.com>
2024-08-26 22:12:38 -0400

    fixed chunk crashing

--- src/server/game.js
+++ src/server/game.js
@@ -42,7 +42,7 @@ class Game {
     addPlayer(socket, username){
         let spawn = this.world.getSpawn();
         this.players[socket.id] = new Player(socket.id, socket, username, spawn.pos.x, spawn.pos.y, 0);
-        this.players[socket.id].chunk = { x: spawn.chunk.x + 2, y: spawn.chunk.y + 2}; // purposefully make chunk off so that new update has load data
+        this.players[socket.id].chunk = { x: spawn.chunk.x + 10, y: spawn.chunk.y + 10}; // purposefully make chunk off so that new update has load data
         this.players[socket.id].socket.emit(Constants.MSG_TYPES.GAME_UPDATE, this.createUpdate(this.players[socket.id]));
         socket.emit(Constants.MSG_TYPES.PLAYER_INSTANTIATED, {
             x: spawn.pos.x,
--- src/server/world/world.js
+++ src/server/world/world.js
@@ -92,7 +92,7 @@ class World {
         // send chunk updates for same chunks
         const updatedcells = [];
         sameChunks.forEach(sc => {
-            const chunk = this.getChunk(sc.x, sc.y, false);
+            const chunk = this.getChunk(sc.x, sc.y, true);
             chunk.cellUpdates.forEach(cellupdate => {
                 console.log(cellupdate);
                 updatedcells.push({
