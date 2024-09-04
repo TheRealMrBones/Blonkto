1d5484e7d08732c0e123a25a427e22e37f8e6bbf
Mr Bones <62398642+TheRealMrBones@users.noreply.github.com>
2024-09-04 15:14:57 -0400

    Added kill command

---
+++ src/server/commands/killCommand.js
@@ -0,0 +1,40 @@
+const Constants = require('../../shared/constants.js');
+const Command = require('./command.js');
+
+const { COMMAND_ARGUMENTS } = Constants;
+
+class KillCommand extends Command{
+    static key = "kill";
+    static op = true;
+    static args = [
+        [COMMAND_ARGUMENTS.KEY],
+        [COMMAND_ARGUMENTS.KEY, COMMAND_ARGUMENTS.PLAYER],
+    ];
+
+    static execute(game, player, tokens){
+        // get parsed tokens (and check for perms)
+        const parsedTokens = this.getParsedTokens(game, player, tokens);
+        if(!parsedTokens){
+            return;
+        }
+        const argIndex = parsedTokens[0];
+
+        // do command based on what args set used
+        switch(argIndex){
+            case 0: {
+                player.dead = true;
+                player.killedby = "the Server";
+                break;
+            };
+            case 1: {
+                const p = parsedTokens[1];
+                p.dead = true;
+                p.killedby = "the Server";
+                this.sendResponse(player, `killed ${p.username}`);
+                break;
+            };
+        }
+    }
+}
+
+module.exports = KillCommand;
