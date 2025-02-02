0c5281052c52f1fedc33445f35c89585674a266d
Mr Bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-02-01 23:14:55 -0500

    forgot to remove unused file

--- src/game/items/items.ts
+++
@@ -1,9 +0,0 @@
-import ItemRegistry from '../registries/itemRegistry';
-import Item from './item';
-
-import Constants from '../../shared/constants';
-const { ASSETS } = Constants;
-
-ItemRegistry.Register("sword", new Item("Sword", 1, null));
-ItemRegistry.Register("pickaxe", new Item("Pickaxe", 1, null));
-ItemRegistry.Register("stone_block", new Item("Stone Block", 64, ASSETS.STONE_BLOCK));
