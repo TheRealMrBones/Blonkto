578b8edc33b2b70651a752ed2d1f31d67a8b764d
Mr Bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-02-23 20:10:52 -0500

    made more things have drops

--- src/game/entities/entityDefinition.ts
+++ src/game/entities/entityDefinition.ts
@@ -1,5 +1,8 @@
 import EventEmitter from "events";
 
+import DropBase from "../items/dropBase.js";
+import Game from "../game.js";
+import NonplayerEntity from "../objects/nonplayerEntity.js";
 import RegistryValue from "../registries/registryValue.js";
 import ComponentHandler from "../components/componentHandler.js";
 
@@ -13,20 +16,31 @@ class EntityDefinition extends ComponentHandler<EntityDefinition> implements Reg
     maxhealth: number;
     scale: number = 1;
     asset: string = ASSETS.MISSING_TEXTURE;
+    drops: DropBase | null = null;
 
     eventEmitter: EventEmitter = new EventEmitter();
 
-    constructor(displayname: string, asset: string | null, maxhealth: number, scale: number | null){
+    constructor(displayname: string, asset: string | null, maxhealth: number, scale: number | null, drops?: DropBase){
         super();
         this.displayname = displayname;
         this.maxhealth = maxhealth;
         if(scale != null) this.scale = scale;
         if(asset != null) this.asset = asset;
+        if(drops !== undefined) this.drops = drops;
+
+        this.eventEmitter.on("death", (entity: NonplayerEntity, game: Game) => {
+            this.dropItems(entity.x, entity.y, game);
+        });
     }
 
     /** Sets this objects identifier to the given key from the registry */
     mapRegistryKey(key: string): void {
         this.name = key;
+    }
+
+    /** Drops this entities types items on an instances death */
+    dropItems(x: number, y: number, game: Game): void {
+        if(this.drops != null) this.drops.drop(x, y, game);
     }
 }
 
--- src/game/world/ceiling.ts
+++ src/game/world/ceiling.ts
@@ -1,5 +1,7 @@
 import RegistryValue from "../registries/registryValue.js";
 import ComponentHandler from "../components/componentHandler.js";
+import DropBase from "../items/dropBase.js";
+import Game from "../game.js";
 
 import Constants from "../../shared/constants.js";
 const { ASSETS } = Constants;
@@ -9,16 +11,23 @@ class Ceiling extends ComponentHandler<Ceiling> implements RegistryValue {
     name: string = "unregistered";
     displayname: string;
     asset: string = ASSETS.MISSING_TEXTURE;
+    drops: DropBase | null = null;
 
-    constructor(displayname: string, asset: string | null){
+    constructor(displayname: string, asset: string | null, drops?: DropBase){
         super();
         this.displayname = displayname;
-        if(asset != null) this.asset = asset;
+        if(asset !== null) this.asset = asset;
+        if(drops !== undefined) this.drops = drops;
     }
 
     /** Sets this objects identifier to the given key from the registry */
     mapRegistryKey(key: string): void {
         this.name = key;
+    }
+
+    /** Drops the item that this ceiling drops on break */
+    break(x: number, y: number, drop: boolean, game: Game): void {
+        if(drop && this.drops != null) this.drops.drop(x + .5, y + .5, game);
     }
 
     // #region serialization
