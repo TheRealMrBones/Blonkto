686af13ff32b5d89414e343d4fb867ac8a4cd0b5
Mr Bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-02-27 16:03:10 -0500

    started porting commands to typescript + registry format

---
+++ src/game/commands/command.ts
@@ -0,0 +1,159 @@
+import Game from "../game";
+import Player from "../objects/player";
+import RegistryValue from "../registries/registryValue";
+
+import Constants from "../../shared/constants.js";
+const { COMMAND_ARGUMENTS } = Constants;
+
+/** Base class for a command that can be run through chat by players in the game */
+class Command implements RegistryValue {
+    private key: string = "unregistered";
+    private op: boolean;
+    private possibleargs: any[][];
+    private executeParsed: (args: any[], player: Player, game: Game) => void;
+    alternativekeys: string[] = [];
+    private customCanExecute: ((player: Player, game: Game) => boolean) | null = null;
+
+    constructor(op: boolean, possibleargs: any[][], executeParsed: (args: any[], player: Player, game: Game) => void, alternativekeys?: string[]){
+        this.op = op;
+        this.possibleargs = possibleargs;
+        this.executeParsed = executeParsed;
+        if(alternativekeys !== undefined) this.alternativekeys = alternativekeys;
+    }
+
+    /** Sets this objects identifier to the given key from the registry */
+    mapRegistryKey(key: string): void {
+        this.key = key;
+    }
+
+    /** Sets the custom can execute to override the default one */
+    overrideCanExecute(customCanExecute: (player: Player, game: Game) => boolean): void {
+        this.customCanExecute = customCanExecute;
+    }
+
+    // #region command execution
+
+    /** Tries to run this command with the given args */
+    execute(rawargs: any[], player: Player, game: Game): void {
+        if(!this.canExecute(player, game))
+            game.chatManager.sendMessageTo(player, "You do not have permission to use this command");
+
+        const args = this.parseArgs(rawargs, game);
+        if(typeof args === "string") // string return is a parsing error
+            game.chatManager.sendMessageTo(player, args);
+        
+        this.executeParsed(args as any[], player, game);
+    }
+
+    /** Returns if the requesting player has permission to execute this command */
+    canExecute(player: Player, game: Game): boolean {
+        if(this.customCanExecute !== null) return this.customCanExecute(player, game);
+        return (!this.op || game.opManager.isOp(player.username));
+    }
+
+    /** Returns the parsed args from the given raw args if they are compatable with this command and returns the error string otherwise */
+    parseArgs(rawargs: any[], game: Game): any[] | string {
+        // parsed args are the possible args but as they get checked the previous values change to the parsed rawargs
+        const parsedargs = [];
+        for(let i = 0; i < this.possibleargs.length; i++){
+            parsedargs.push([...this.possibleargs[i]]);
+        }
+        let error = "Command Failed";
+
+        // read inputed args to find correct args
+        for(let i = 0; parsedargs.length > 0; i++){
+            // end of inputed args
+            if(rawargs.length == i){
+                const correctrawargs = parsedargs.find(tkns => tkns.length == i);
+                if(correctrawargs){
+                    // correct args!
+                    return correctrawargs;
+                }else{
+                    // too few args for available commands
+                    error = `Incorrect arguments for command: ${this.key}`;
+                    break;
+                }
+            }
+
+            // check each possible arg if next raw arg is valid
+            for(let j = 0; j < parsedargs.length; j++){
+                if(parsedargs[j].length == i){
+                    error = `Incorrect arguments for command: ${this.key}`;
+                    parsedargs.splice(j, 1);
+                    j--;
+                    continue;
+                }
+
+                switch(parsedargs[j][i]){
+                    case COMMAND_ARGUMENTS.KEY: {
+                        // always set first arg (key) to the index of the original args (for use in actual command)
+                        parsedargs[j][i] = j;
+                        break;
+                    }
+                    case COMMAND_ARGUMENTS.PLAYER: {
+                        const p = Object.values(game.players).find(p => p.username.toLowerCase() == rawargs[i].toLowerCase());
+                        if(!p){
+                            error = `Player "${rawargs[i]}" does not exist`;
+                            parsedargs.splice(j, 1);
+                            j--;
+                        }else{
+                            parsedargs[j][i] = p;
+                        }
+                        break;
+                    }
+                    case COMMAND_ARGUMENTS.INT: {
+                        if(isNaN(rawargs[i])){
+                            error = `"${rawargs[i]}" is not an integer`;
+                            parsedargs.splice(j, 1);
+                            j--;
+                        }else{
+                            parsedargs[j][i] = parseInt(rawargs[i]);
+                        }
+                        break;
+                    }
+                    case COMMAND_ARGUMENTS.FLOAT: {
+                        if(isNaN(rawargs[i])){
+                            error = `"${rawargs[i]}" is not a float`;
+                            parsedargs.splice(j, 1);
+                            j--;
+                        }else{
+                            parsedargs[j][i] = parseFloat(rawargs[i]);
+                        }
+                        break;
+                    }
+                    case COMMAND_ARGUMENTS.BOOLEAN: {
+                        switch(rawargs[i]){
+                            case "true":
+                            case "t":
+                            case "1":
+                                parsedargs[j][i] = true;
+                                break;
+                            case "false":
+                            case "f":
+                            case "0":
+                                parsedargs[j][i] = false;
+                                break;
+                            default:
+                                error = `"${rawargs[i]}" is not a boolean`;
+                                parsedargs.splice(j, 1);
+                                j--;
+                                break;
+                        }
+                        break;
+                    }
+                    case COMMAND_ARGUMENTS.STRING: {
+                        parsedargs[j][i] = rawargs[i];
+                        break;
+                    }
+                }
+            }
+        }
+
+        // if no correct args found send most recent error
+        return error;
+    }
+
+    // #endregion
+}
+
+export default Command;
