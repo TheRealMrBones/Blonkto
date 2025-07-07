4976e8bbe7e49867710bda4f8619892a47d214e7
mr bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-07-07 14:34:23 -0400

    entitycount command for specific type

--- src/game/commands/entityCountCommand.ts
+++ src/game/commands/entityCountCommand.ts
@@ -8,10 +8,24 @@ const { COMMAND_ARGUMENTS } = Constants;
 
 const args = [
     [COMMAND_ARGUMENTS.KEY],
+    [COMMAND_ARGUMENTS.KEY, COMMAND_ARGUMENTS.STRING],
 ];
 
 export default (): void => CommandRegistry.register("entitycount", new CommandDefinition(true, args, entityCountCommand, "Gets the amount of entities currently loaded in the world"));
 
 function entityCountCommand(args: any[], player: Player, game: Game){
-    game.chatManager.sendMessageTo(player, game.performanceManager.getEntityCounts());
+    const argIndex = args[0];
+
+    switch(argIndex){
+        case 0: {
+            game.chatManager.sendMessageTo(player, game.performanceManager.getEntityCounts());
+            break;
+        };
+        case 1: {
+            const definition = args[1];
+            const count = game.entityManager.getNonplayerEntities().filter(e => e.definition.getRegistryKey() == definition).length;
+            game.chatManager.sendMessageTo(player, `Entities of type "${definition}": ${count}`);
+            break;
+        };
+    }
 }
