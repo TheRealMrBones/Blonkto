57094c5d955bd8d2e4bb6de4da0824bf7103053d
mr bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-07-24 15:06:17 -0400

    cave generation

--- src/game/world/generation/caveLayerGenerator.ts
+++ src/game/world/generation/caveLayerGenerator.ts
@@ -32,7 +32,44 @@ class CaveLayerGenerator implements ILayerGenerator {
             }
         }
 
+        const cavecheckdist = 1;
+        for(let dx = -cavecheckdist; dx <= cavecheckdist; dx++){
+            for(let dy = -cavecheckdist; dy <= cavecheckdist; dy++){
+                this.generateCaves(chunk, chunkx + dx, chunky + dy, game);
+            }
+        }
+
         return chunk;
+    }
+
+    // Generates caves in the requested other chunk and does cell operations on the given chunk
+    generateCaves(chunk: Chunk, otherchunkx: number, otherchunky: number, game: Game): void {
+        const seed = multiNumberHash(otherchunkx, otherchunky, chunk.layer.seed);
+        const rng = new SeededRandom(seed);
+
+        for(let dx = 0; dx < CHUNK_SIZE; dx++){
+            for(let dy = 0; dy < CHUNK_SIZE; dy++){
+                if(rng.next() < .0025) this.generateCave(chunk, rng, dx + otherchunkx * CHUNK_SIZE, dy + otherchunky * CHUNK_SIZE, game);
+            }
+        }
+    }
+
+    // Generates cave with the given rng at the requested pos and does cell operation on the given chunk
+    generateCave(chunk: Chunk, rng: SeededRandom, cavex: number, cavey: number, game: Game): void {
+        const size = rng.nextInt(2, 6);
+
+        for(let dx = -size; dx <= size; dx++){
+            for(let dy = -size; dy <= size; dy++){
+                const dist = Math.sqrt(dx * dx + dy * dy);
+                if(dist > size + .5) continue;
+                
+                const relativex = cavex + dx - chunk.chunkx * CHUNK_SIZE;
+                const relativey = cavey + dy - chunk.chunky * CHUNK_SIZE;
+                if(relativex < 0 || relativex >= CHUNK_SIZE || relativey < 0 || relativey >= CHUNK_SIZE) continue;
+
+                chunk.cells[relativex][relativey].setBlock(null, game);
+            }
+        }
     }
 }
 
