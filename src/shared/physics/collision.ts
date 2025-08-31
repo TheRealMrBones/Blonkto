ff120ff6cf14ee8f3f30ebc2a0d39210997633fa
mr bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-08-30 21:52:28 -0400

    better get axes

--- src/shared/physics/collision.ts
+++ src/shared/physics/collision.ts
@@ -1,11 +1,9 @@
 import CollisionObject from "./collisionObject.js";
+import Vector2D from "./vector2d.js";
 
 /** checks if the two given collision objects are colliding using SAT */
 export function checkCollision(object1: CollisionObject, object2: CollisionObject): boolean {
-    const axes = [
-        ...object1.getSeperateAxisTheoremTestAxes(),
-        ...object2.getSeperateAxisTheoremTestAxes(),
-    ];
+    const axes = getTestAxes(object1, object2);
 
     for(const axis of axes){
         const range1 = object1.getSeperateAxisTheoremRange(axis);
@@ -16,4 +14,22 @@ export function checkCollision(object1: CollisionObject, object2: CollisionObjec
     }
 
     return true;
+}
+
+/** Gets all of the axes to check for the SAT between two collision objects */
+function getTestAxes(object1: CollisionObject, object2: CollisionObject): Vector2D[] {
+    const axes = [
+        ...object1.getSeperateAxisTheoremTestAxes(),
+        ...object2.getSeperateAxisTheoremTestAxes(),
+    ];
+
+    // get axis for closest points of circles
+    if(axes.length == 0){
+        axes.push(new Vector2D(
+            object1.position.x - object2.position.x,
+            object1.position.y - object2.position.y
+        ).getOrthogonal().getUnitVector());
+    }
+
+    return axes;
 }
