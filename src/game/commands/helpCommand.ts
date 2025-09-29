912bf3f328bca2433832c156e973323de6ecd404
mr bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-09-28 23:22:25 -0400

    help can describe specific command

--- src/game/commands/helpCommand.ts
+++ src/game/commands/helpCommand.ts
@@ -9,9 +9,10 @@ const { COMMAND_ARGUMENTS } = Constants;
 const args = [
     [COMMAND_ARGUMENTS.KEY],
     [COMMAND_ARGUMENTS.KEY, COMMAND_ARGUMENTS.INT],
+    [COMMAND_ARGUMENTS.KEY, COMMAND_ARGUMENTS.STRING],
 ];
 
-export default (): void => CommandRegistry.register(new CommandDefinition("help", false, args, helpCommand, "Gives list of commands"));
+export default (): void => CommandRegistry.register(new CommandDefinition("help", false, args, helpCommand, "Gives list of commands or describes a specified command"));
 
 function helpCommand(args: any[], player: Player, game: Game){
     const argIndex = args[0];
@@ -20,14 +21,23 @@ function helpCommand(args: any[], player: Player, game: Game){
         .filter(c => !(c.getOp() && !game.playerManager.opManager.isOp(player.username)))
         .sort((a: CommandDefinition, b: CommandDefinition) => a.key.localeCompare(b.key));
 
-    const perpage = 10;
-    const pages = Math.ceil(commands.length / perpage);
-    const page = argIndex == 1 ? Math.max(Math.min(args[1], pages), 1) : 1;
+    if(argIndex == 0 || argIndex == 1){
+        const perpage = 10;
+        const pages = Math.ceil(commands.length / perpage);
+        const page = argIndex == 1 ? Math.max(Math.min(args[1], pages), 1) : 1;
 
-    game.chatManager.sendMessageTo(player, `command list page ${page}/${pages}:`);
+        game.chatManager.sendMessageTo(player, `command list page ${page}/${pages}:`);
 
-    for(let i = (page - 1) * perpage; i < page * perpage && i < commands.length; i++){
-        const command = commands[i];
-        game.chatManager.sendMessageTo(player, `- ${command.key} - ${command.getHelp()}`);
+        for(let i = (page - 1) * perpage; i < page * perpage && i < commands.length; i++){
+            const command = commands[i];
+            game.chatManager.sendMessageTo(player, `- ${command.key} - ${command.getHelp()}`);
+        }
+    }else{
+        if(CommandRegistry.has(args[1])){
+            const command = CommandRegistry.get(args[1]);
+            game.chatManager.sendMessageTo(player, `${command.key} - ${command.getHelp()}`);
+        }else{
+            game.chatManager.sendMessageTo(player, `"${args[1]}" is not a command`);
+        }
     }
 }
