2e46ec51810626417a8056787bd1e7d8373f19e8
mr bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-10-17 16:58:23 -0400

    squares dont check duplicate normals

--- src/shared/physics/square.ts
+++ src/shared/physics/square.ts
@@ -1,5 +1,6 @@
 import { Vector2D } from "../types.js";
 import Polygon from "./polygon.js";
+import V2D from "./vector2d.js";
 
 /** A uniform square in 2d space */
 class Square extends Polygon {
@@ -11,6 +12,7 @@ class Square extends Polygon {
         this.halfwidth = width / 2;
     }
 
+    /** Returns the vertices of this polygon relative to the origin of the polygon */
     getVeticesFromOrigin(): Vector2D[] {
         return [
             [-this.halfwidth, -this.halfwidth],
@@ -20,8 +22,23 @@ class Square extends Polygon {
         ];
     }
 
+    /** Returns the vertex count of this square */
     getVertexCount(): number {
         return 4;
+    }
+
+    /** Returns the unit normal vector of each edge of this square */
+    override getNormals(): Vector2D[] {
+        const vertices = this.getVertices();
+        vertices.push(vertices[0]);
+
+        const normals: Vector2D[] = [];
+        for(let i = 0; i < 2; i++){
+            const edge = V2D.subtract(vertices[i + 1], vertices[i]);
+            normals.push(V2D.getUnitVector(V2D.getOrthogonal(edge)));
+        }
+
+        return normals;
     }
 }
 
